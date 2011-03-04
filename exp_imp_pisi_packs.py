f198d85e628c7b9afd39be7dc24de67de13b1d50
Mahmud Çelenlioğlu <sadican@sdcn.com>
2011-03-04 13:17:47 +0200

    modüller eklendi

---
+++ exp_imp_pisi_packs.py
@@ -0,0 +1,18 @@
+from pisi.db.installdb import InstallDB
+
+def export_installed_packages():
+  """ It exports packages to a file named installed_packages.lst """
+  ipdb = InstallDB()
+  installed_packages = ipdb.list_installed()
+  installed_packages.sort()
+  f = open("installed_packages.lst", "w")
+  map(lambda x: f.write("%s\n" % x), installed_packages)
+  f.close()
+
+def import_installed_packages(imported_list):
+  """ It imports packages to a list named imported_list from file named installed_packages.lst """
+  source_file = "installed_packages.lst"
+  f = open(source_file, "r")
+  map(lambda x: imported_list.append(x.strip("\n")), f)
+  f.close()
+  return imported_list
