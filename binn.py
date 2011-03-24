8458a802ecb913694f4746c54b4bd2b3bc129b09
Mahmud Çelenlioğlu <sadican@sdcn.com>
2011-03-25 01:18:19 +0200

    ana program tekrar yazıldı

--- binn.py
+++ binn.py
@@ -1,70 +1,66 @@
 #!/usr/bin/python
 #-*-coding: utf-8-*-
 """ --- Binn --- """
-from exp_imp_pisi_packs import import_installed_packages
-from exp_imp_pisi_packs import export_installed_packages
-from install_pisi_packs import install_user_packs
-from update_repos import update_pisi_repositories
-from upgrade_packs import upgrade_pisi_packs
-
-def extract_pack():
-  """ Gets installed Pisi packages by user named userpacks """
-  print "Starting..."
-  export_installed_packages()
-  imported_list = []
-  import_installed_packages(imported_list)
-
-  corepacks = open("corepacks","r")#put core packages into another list
-  corep = []
-  for line in corepacks:
-    packname = line.split()
-    corep.append(packname[0])
-  corepacks.close()
-
-  userpacks = open("userpacks.lst","w")
-  #compare lists  
-  for line in imported_list:
-    if line not in corep:
-      userpacks.write(line+"\n")
-  userpacks.close()
-  print "Finished!"
+import os
+from extract_list import extract_list
+from pisi.util import colorize
 
 def menuPrinting():
   """ User menu of Binn application """
-  print '=' * 9 + ' MENU ' + '=' * 9
-  print '1-Update repository'
-  print '2-Update Pisi'
-  print '3-Extract package list from Pisi'
-  print '4-Setup packages'
-  print '0-Exit'
-  print '=' * 24
+  print colorize('=' * 9 + ' MENU ' + '=' * 9,"yellow")
+  print colorize('1-Update repository',"yellow")
+  print colorize('2-Update Pisi',"yellow")
+  print colorize('3-Extract package list from Pisi',"yellow")
+  print colorize('4-Setup packages',"yellow")
+  print colorize('0-Exit',"yellow")
+  print colorize('=' * 24,"yellow")
 
 def executeCode(ch):
   """ Menu choices """
   if ch == 1:
-    update_pisi_repositories()
+    os.system("sudo pisi ur")
+    print "\n"
+    
   elif ch == 2:
-    upgrade_pisi_packs()
+    os.system("sudo pisi up")
+    print "\n"
+    
   elif ch == 3:
-    extract_pack()
+    extract_list()
+    print "\n"
+    
   elif ch == 4:
-    install_user_packs()
+    try:
+      f = open("userpacks.lst","r")
+      
+      for line in f:
+	os.system("sudo pisi it " + line)
+      
+      f.close()
+    except IOError:
+      print colorize("No such file named userpacks.lst in the same directory!","cyan")
+    print "\n"
+      
   elif ch == 0:
     exit()
 
-choice = 1
-# menu
-while choice != 0:
-  menuPrinting()
-  choice = raw_input("Select one of them!\t")
-  upperchoice=4
-  lowerchoice=0
+def main_program():
+  choice = 1
+  
+  while choice != 0:
+    """ Menu loop """
+    menuPrinting()
+    choice = raw_input("Select one of them!\t")
+    upperchoice=4
+    lowerchoice=0
+    
+    if choice.isdigit() != True:
+      print colorize("WRONG! Enter a NUMBER!\n","cyan")
+      continue
+    elif int(choice) > upperchoice or int(choice) < lowerchoice:
+      print colorize("WRONG! Enter a VALID number!\n","cyan")
+      continue
+    else:
+      executeCode(int(choice))
 
-  if choice.isdigit() != True:
-    print "WRONG! Enter a NUMBER!\n"
-    continue
-  elif int(choice) > upperchoice or int(choice) < lowerchoice:
-    print "WRONG! Enter a VALID number!\n"
-    continue
-  else:
-    executeCode(int(choice))
+main_program()
